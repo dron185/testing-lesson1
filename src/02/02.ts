05d5549fc2b8e53d637f8e75add0ffa115671fc8
Andrei Huryn <gurin.nord@gmail.com>
2024-03-06 18:20:18 +0300

    feat: complete lesson2

--- src/02/02.ts
+++ src/02/02.ts
@@ -1,5 +1,27 @@
-const student = {
-    "name": "Dimych",
+type LocalCityType = {
+    title: string
+    countryTitle: string
+}
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+type TechType = {
+    id: number
+    title: string
+}
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+const student: StudentType = {
+    id: 1,
+    name: "Dimych",
     age: 32,
     isActive: false,
     address: {
